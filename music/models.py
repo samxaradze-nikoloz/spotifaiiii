e8e86e565ead43e59fc0a2d9263a3b2e53d01b01
M4YH3M <tengodzebisashvili6@gmail.com>
2025-12-09 15:43:29 +0400

    sdasda

--- music/models.py
+++ music/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.db import models
 
 
 class Artist(models.Model):
     name = models.CharField(max_length=200)
+    country = models.CharField(max_length=100 , default='Unknown')
+    popularity = models.IntegerField(default=0)  
+    followers = models.PositiveIntegerField(default=0)
+
     def __str__(self):
         return self.name
 
@@ -10,14 +15,21 @@ class Artist(models.Model):
 class Album(models.Model):
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name="albums")
     title = models.CharField(max_length=200)
+    release_year = models.PositiveIntegerField(null=True, blank=True)
+    total_tracks = models.PositiveIntegerField(default=0)
+
     def __str__(self):
         return self.title
-
 
 
 class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE, related_name="songs")
     title = models.CharField(max_length=200)
+    duration_seconds = models.PositiveIntegerField( default=0)  
+    track_number = models.PositiveIntegerField(default=1)
+    explicit = models.BooleanField(default=False)
+    popularity = models.IntegerField(default=0)  
+
     def __str__(self):
         return self.title
 
